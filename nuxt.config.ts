74c6d4d0acaa65a53f7555dcda920f1af3758ce1
Daniel Lopez <daniellopezj0327@gmail.com>
2024-03-01 07:02:06 -0500

    change default start

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  typescript: {
+    strict: true,
+  },
+  app: {
+    head: {
+      title: 'Zenoga Daniel',
+    },
+  },
+  srcDir: 'src/',
 })
---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Hola buen dia
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
