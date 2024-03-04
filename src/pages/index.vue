3cde48c0ee052d3128f0037c0ed278c950f503f3
Daniel Lopez <daniellopezj0327@gmail.com>
2024-03-04 15:02:15 -0500

    add meta tags

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,43 @@
 <template>
+    <Head>
+    <Title>Zenoga Daniel López</Title>
+    <Meta name="author" content="Daniel López" />
+    <Meta
+      name="description"
+      content="Zemoga interview  Daniel López"
+    />
+    <Meta
+      property="og:title"
+      content="Zemoga  Daniel López"
+    />
+    <Meta
+      property="og:description"
+      content="Zemoga interview  Daniel López"
+    />
+    <Meta
+      property="og:image"
+      content="https://zemoga-daniel-lopez-daniellopezj01.vercel.app/img/main.webp"
+    />
+    <Meta property="og:url" content="https://zemoga-daniel-lopez-daniellopezj01.vercel.app/" />
+    <Meta name="twitter:card" content="summary_large_image" />
+    <Meta
+      name="twitter:title"
+      content="Zemoga Daniel López"
+    />
+    <Meta
+      name="twitter:description"
+      content="Zemoga interview Daniel López"
+    />
+    <Meta
+      name="twitter:image"
+      content="https://zemoga-daniel-lopez-daniellopezj01.vercel.app/img/main.webp"
+    />
+    <Meta name="twitter:url" content="https://zemoga-daniel-lopez-daniellopezj01.vercel.app/" />
+    <Meta
+      name="keywords"
+      content="Zemoga, JavaScript, html, css, vue, nuxt"
+    />
+  </Head>
   <Header />
   <Nav />
   <div class="max-centered">
@@ -11,6 +50,15 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: 'canonical',
+      href: 'https://zemoga-daniel-lopez-daniellopezj01.vercel.app/',
+    },
+  ],
+});
+</script>
 
 <style scoped></style>
